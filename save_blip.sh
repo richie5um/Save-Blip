f82e23fd01b1b75c1d3f9fea967886c996696685
mrhazelh <timread@btinternet.com>
2015-03-21 16:43:34 +0000

    Change of functionality to allow users to print their Blip comments with their entries. This requires the user to provide an additional command line parameter.

--- save_blip.sh
+++ save_blip.sh
@@ -2,9 +2,9 @@
 ##############################################
 #
 # This program is provided on an 'as-is' basis.
-# No support is offered for any defects found
+# No support is offered for any defects found 
 # in this program.
-# No liability is accepted for any damage
+# No liability is accepted for any damage 
 # caused through the use of this program.
 #
 ##############################################
@@ -18,74 +18,82 @@
 # additional line breaks!
 # Then make the file executable:
 # chmod +x what_ever_you_called_the_file
-#
+# 
 # Now you should be able to run the program.
 # ./what_ever_you_called_the_file
-# The program will then tell you the
+# The program will then tell you the 
 # parameters it needs.
 #
-# The program will create a subdirectory
+# The program will create a subdirectory 
 # within the directory you save this script
-# to hold all the entries generated.
+# to hold all the entries generated. 
 ##############################################
 
+
 ##############################################
-#
-# ******* NOTE *********
+# 
+#         ******* NOTE ********* 
 # This code relies on two programs other than
-# the standard Unix command set, such as
+# the standard Unix command set, such as 
 # mkdir, sed, grep, rm.
 #
 # These two commands are: wkhtmltopdf and wget
 # These two commands *must* be installed and
-# in a directory listed in your shell PATH
+# in a directory listed in your shell PATH 
 # variable.
 #
-# You can check this from a terminal prompt as
+# You can check this from a terminal prompt as 
 # follows ($ is just the terminal prompt):
 # $ wkhtmltopdf -V
 # wkhtmltopdf 0.12.2.1 (with patched qt)
 # $ wget -V
 # GNU Wget 1.15 built on linux-gnu.
-# ... etc
+#  ... etc
 #
 # You will need to install version 0.12.2.1 for
 # the print out to be faithful to the Blip page
 # layout. You can download that version from
 # http://wkhtmltopdf.org/downloads.html
-#
+# 
 # Bear in mind that you have to trust that this
 # download is safe!
 #
 ##############################################
 
-if [[ $# -lt 2 ]]; then
-echo "There are two forms of usage."
-echo "	The first prints all entries in reverse chronological order"
-echo "	given an initial entry"
-echo ""
-echo "	Usage: blip_username url_of_first_blip_to_print"
-echo "	Example:"
-echo "	$0 yourusername https://www.polaroidblipfoto.com/entry/1234567890 "
-echo ""
-echo "	The second prints entries from an initial entry up to, but not"
-echo "	including, a final entry in reverse chronological order."
-echo "	No check is made that the last entry is before the first."
-echo ""
-echo "	Usage: blip_username url_of_first_blip_to_print url_of_final_blip_to_stop_at"
-echo "	Example:"
-echo "	$0 yourusername https://www.polaroidblipfoto.com/entry/4321 https://www.polaroidblipfoto.com/entry/1234"
-exit 1
+if [[ $# -lt 3 ]]; then
+	echo "There are two forms of usage."
+	echo "	The first prints all entries in reverse chronological order"
+	echo "	given an initial entry"
+	echo ""
+	echo "	Usage: blip_username with_comments url_of_first_blip_to_print"
+	echo "	Example:"
+	echo "	$0 yourusername y https://www.polaroidblipfoto.com/entry/1234567890 "
+	echo ""
+	echo "	The second prints entries from an initial entry up to, but not"
+	echo "	including, a final entry in reverse chronological order."
+	echo "	No check is made that the last entry is before the first."
+	echo ""
+	echo "	Usage: blip_username with_comments url_of_first_blip_to_print url_of_final_blip_to_stop_at"
+	echo "	Example:"
+	echo "	$0 yourusername n https://www.polaroidblipfoto.com/entry/4321 https://www.polaroidblipfoto.com/entry/1234"
+	exit 1
 fi
 
 base_url="https://www.polaroidblipfoto.com"
 user=$1
-previous_url=$2
-final_url=$3
+comments=$2
+previous_url=$3
+final_url=$4
 tmp_file=/tmp/blip$$
+additional_javascript=""
 
 if [[ -z ${final_url} ]]; then
-final_url=${base_url}
+	final_url=${base_url}
+fi
+
+if [[ $comments != "y" && ${comments} != "n" ]]; then
+	echo "Comments option must be y or n"
+	exit 1
 fi
 
 # Use a 2 second delay between getting entries.
@@ -94,33 +102,58 @@ delay=2
 blip_entries_dir=./blip_entries
 
 if [[ ! -d ${blip_entries_dir} ]]; then
-echo "Making directory ${blip_entries_dir}"
-mkdir -p ${blip_entries_dir}
-if [[ $? != 0 ]]; then
-echo "Failed to make directory ${blip_entries_dir}"
-exit 1
-fi
+	echo "Making directory ${blip_entries_dir}"
+	mkdir -p ${blip_entries_dir}
+	if [[ $? != 0 ]]; then
+		echo "Failed to make directory ${blip_entries_dir}"
+		exit 1
+	fi
 fi
 
 # Start by printing a front cover
 
 echo "Printing front cover for user ${user}...."
 wkhtmltopdf -q ${base_url}/${user} \
-./${blip_entries_dir}/front_cover.pdf \
-> /dev/null 2>&1
+	./${blip_entries_dir}/front_cover.pdf  \
+	> /dev/null 2>&1
 
 while [[ $previous_url != $final_url ]];
 do
-wget -q -O ${tmp_file} $previous_url
-entry_date=$( grep 'JournalGallery","title":"' ${tmp_file} \
-| sed 's/^.*JournalGallery","title":"//' \
-| sed 's/".*$//' | sed 's/ /_/g' )
-echo "Printing entry ${entry_date}...."
-wkhtmltopdf -q ${previous_url} \
-./${blip_entries_dir}/${entry_date}.pdf \
-> /dev/null 2>&1
-previous_url=${base_url}$( grep 'title="Previous"' ${tmp_file} \
-| sed 's/^.*href="//' | sed 's/".*$//' )
-rm ${tmp_file}
-sleep ${delay}
+	wget -q -O ${tmp_file} $previous_url
+	entry_date=$( grep 'JournalGallery","title":"' ${tmp_file} \
+		| sed 's/^.*JournalGallery","title":"//' \
+		| sed 's/".*$//' | sed 's/ /_/g' )
+	echo "Printing entry ${entry_date}...."
+	if [[ $comments == "n" ]]; then
+		wkhtmltopdf -q ${additional_javascript} ${previous_url} \
+			${blip_entries_dir}/${entry_date}.pdf  \
+			> /dev/null 2>&1
+	else	
+		result=""
+		while [[ -z $result ]];
+		do
+			# Note
+			# The run-script function does not always work correctly
+			# hence the loop.
+			result=$( wkhtmltopdf ${previous_url} \
+				--no-stop-slow-scripts \
+				--run-script 'console.log(document.readyState);' \
+				--run-script  'document.onload = load_comments.click();' \
+				--run-script  'load_comments.click();' \
+				--run-script  'load_comments.scrollIntoView(true);' \
+				--run-script  'load_comments.click();' \
+				--javascript-delay 2000  \
+				--debug-javascript  \
+				${blip_entries_dir}/${entry_date}.pdf 2>&1 \
+				| grep "complete" )
+			if [[ -z $result ]]; then
+				echo "Retrying...."
+				sleep 1
+			fi
+		done
+	fi
+	previous_url=${base_url}$( grep 'title="Previous"' ${tmp_file} \
+		| sed 's/^.*href="//' | sed 's/".*$//' )
+	rm ${tmp_file}
+	sleep ${delay}
 done
